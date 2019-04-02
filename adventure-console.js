8eb562adf828c090fdc17a527a07886933e2e018
Jonathan Jones <jones.jonathan.david@gmail.com>
2019-04-02 11:10:54 -0400

    another attempt at loading json

--- adventure-console.js
+++ adventure-console.js
@@ -6,20 +6,6 @@ var _ = require('lodash');
 const fetch = require('node-fetch');
 
 
-var possible_actions = {'login': 1};
-var results;
-
-
-const returnJson = (response) => response.json();
-
-const file_name = "demo.json";
-const local_path = 'http://localhost:3000'
-const chapters_url = local_path + '/chapters/' + file_name;
-fetch(chapters_url)
-    .then(returnJson)
-    .then(function(json_obj){results = json_obj});
-
-
 function serveHome (req, res) {
   let file_name = "console.html";
   let local_path = __dirname;
@@ -55,10 +41,21 @@ function computeResult (results, possible_actions, command) {
 }
 
 
+async function getValue(object, key) {
+  let value = await object[key];
+  return value
+}
+
+
 function sendResponseBase (results, possible_actions, command) {
   let result;
   let hint;
   let result_key = possible_actions[command];
+  let result_value = getValue(results, result_key);
+  console.log(result_value);
+  console.log(result_key);
+  // console.log(Object.keys(results));
+  console.log(results);
   [result, hint, possible_actions] = results[result_key];
 
   let command_monitor = makeMonitorStr(command);
@@ -68,13 +65,28 @@ function sendResponseBase (results, possible_actions, command) {
   io.emit('console_output', response);
 }
 
-
 function onConnect (socket) {
   logConnect();
   sendResponse('login');
   socket.on('prompt_input', sendResponse);
   socket.on('disconnect', logDisconnect);
 }
+
+
+// var results;
+
+async function loadJson (url) {
+  let response = await fetch(url);
+  return await response.json();
+}
+
+
+var possible_actions = {'login': '1'};
+
+const file_name = "demo.json";
+const local_path = 'http://localhost:3000'
+const chapter_url = local_path + '/chapters/' + file_name;
+const results = loadJson(chapter_url);
 
 const sendResponse = _.partial(sendResponseBase, results, possible_actions);
 
